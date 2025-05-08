2811d9b20e12dfa508af499dc0d72da4060c234b
Mathias Ögren <166627481+Battl3Junk1e@users.noreply.github.com>
2025-05-08 17:01:02 +0200

    added functionality

--- get_img.py
+++ get_img.py
@@ -2,9 +2,9 @@ import requests
 
 url = "https://axmjqhyyjpat.objectstorage.eu-amsterdam-1.oci.customer-oci.com/n/axmjqhyyjpat/b/randomimages/o/cars%2F"
 
-for _ in range(1,5):
+for _ in range(1,211):
     r = requests.get(url +str(_)+".png")
-    with open(f"image{_}.png", "wb") as image:
+    with open(f"images/image{_}.png", "wb") as image:
         image.write(r.content)
         if r.status_code == 404:
             break
