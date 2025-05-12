a543a01c259f583860cb90ed6bc60954f1518903
Alex Bramstång <alex.bramstang@live.se>
2025-05-12 21:01:55 +0200

    Added SQL View Script

--- sqlscripts/InvoiceLinesMarginView.sql
+++ sqlscripts/InvoiceLinesMarginView.sql
@@ -1,3 +1,14 @@
 USE [WideWorldImporters]
-Select *
-From sales.customers
+GO
+
+CREATE VIEW dbo.InvoiceLineMargins
+AS
+    SELECT 
+        si.InvoiceID, 
+        sc.CustomerName, 
+        si.InvoiceDate, 
+        sil.Description, 
+        sil.LineProfit
+    FROM Sales.Invoices AS si
+    JOIN Sales.InvoiceLines AS sil ON si.InvoiceID = sil.InvoiceID
+    JOIN Sales.Customers AS sc ON si.CustomerID = sc.CustomerID;
